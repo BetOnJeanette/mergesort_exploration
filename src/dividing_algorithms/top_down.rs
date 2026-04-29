f49d6f2f2ad657bae4c6b5e79332fa3392aebaa9
BetOnJeanette <jeanettecrull99@gmail.com>
2026-04-29 17:28:13 -0400

    Actually merge

--- src/dividing_algorithms/top_down.rs
+++ src/dividing_algorithms/top_down.rs
@@ -1,12 +1,15 @@
-pub fn merge_sort(list: &mut Vec<i32>) {
+use crate::merging_algorithms::Merger;
+
+pub fn merge_sort(list: &mut Vec<i32>, merge: Merger) {
     let center = list.len() / 2;
-    power_merge_top_down_helper(list, 0, center, list.len()); 
+    merge_sort_helper(list, 0, center, list.len(), merge); 
 }
 
-fn power_merge_top_down_helper(list: &mut Vec<i32>, left: usize, right: usize, end: usize){
+fn merge_sort_helper(list: &mut Vec<i32>, left: usize, right: usize, end: usize, merge: Merger){
     if left >= right || right >= end { return; }
     let left_center = (left + right) / 2;
     let right_center = (right + end) / 2;
-    power_merge_top_down_helper(list, left, left_center, right);
-    power_merge_top_down_helper(list, right, right_center, end);
+    merge_sort_helper(list, left, left_center, right, merge);
+    merge_sort_helper(list, right, right_center, end, merge);
+    merge(list, left, right, end);
 }
