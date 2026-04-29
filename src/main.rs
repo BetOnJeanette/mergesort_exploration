3d85e5d52ce7960c1db8052441f2f1608cc03d9b
BetOnJeanette <jeanettecrull99@gmail.com>
2026-04-28 22:35:47 -0400

    print results in main instead of the lib itself

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,16 @@ fn main() {
     let mut rng = rand::rng();
     let mut vec: Vec<i32> = (0..2048).collect();
     vec.shuffle(&mut rng);
+    print!("before: [",);
+    for num in vec.clone() {
+        print!("{}, ", num.to_string());
+    }
+    println!("]");
     powermerge::power_merge(&mut vec);
+    print!("after: [",);
+    for num in vec.clone() {
+        print!("{}, ", num.to_string());
+    }
+    println!("]");
 }
 
--- src/powermerge/mod.rs
+++ src/powermerge/mod.rs
@@ -4,9 +4,19 @@ pub fn power_merge(list: &mut Vec<i32>) {
         build_larger_sublists(list, sub_size);
         sub_size *= 2;
     }
-    for num in list.clone() {
-        print!("{}, ", num.to_string());
-    }
+}
+
+pub fn power_merge_top_down(list: &mut Vec<i32>) {
+    let center = list.len() / 2;
+    power_merge_top_down_helper(list, 0, center, list.len()); 
+}
+
+fn power_merge_top_down_helper(list: &mut Vec<i32>, left: usize, right: usize, end: usize){
+    if left >= right || right >= end { return; }
+    let left_center = (left + right) / 2;
+    let right_center = (right + end) / 2;
+    power_merge_top_down_helper(list, left, left_center, right);
+    power_merge_top_down_helper(list, right, right_center, end);
 }
 
 fn build_larger_sublists(list: &mut Vec<i32>, cur_sub_size: usize){
