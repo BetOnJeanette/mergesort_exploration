85871f8b654c240bdaabc53438439c320184f684
BetOnJeanette <jeanettecrull99@gmail.com>
2026-04-29 18:30:58 -0400

    Implement random inputs per-iteration on test

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,13 +1,22 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use power_merge::powermerge;
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::hint::black_box;
+use power_merge::dividing_algorithms::{top_down, bottom_up};
+use power_merge::merging_algorithms::quicksort_like::quicksort_like_merge;
 use rand::prelude::*;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    let mut vec: Vec<i32> = (0..16).collect();
     let mut rng = rand::rng();
-    vec.shuffle(&mut rng);
+    let mut group = c.benchmark_group("Merge opts");
+    for i in [16, 32, 64, 128, 256, 512, 1024, 2048].iter() {
+        let mut vec: Vec<i32> = (0..*i).collect();
+        vec.shuffle(&mut rng);
+        group.bench_with_input(BenchmarkId::new("Bottom Up Quicksort-like", i), &vec.clone(), |b, v| b.iter_batched(|| {
+                let mut out = v.clone();
+                out.shuffle(&mut rng);
+                return out;
+            }, |mut v| bottom_up::merge_sort(black_box(&mut v), quicksort_like_merge),
+            criterion::BatchSize::SmallInput));
 
-    c.bench_function("powermerge", |b| b.iter(|| powermerge::power_merge(&mut vec.clone())));
 }
 criterion_group!(benches, criterion_benchmark);
 criterion_main!(benches);
