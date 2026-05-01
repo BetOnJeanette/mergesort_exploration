e11f44f31b3e2757b0dd430ecc108a2841d3d710
BetOnJeanette <jeanettecrull99@gmail.com>
2026-04-30 22:24:09 -0400

    Add quicksort

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,29 @@
 pub mod merging_algorithms;
 pub mod dividing_algorithms;
+
+pub fn quicksort(list: &mut Vec<i32>){
+    quicksort_helper(list, 0, list.len());
+}
+
+fn quicksort_helper(list: &mut Vec<i32>, start: usize, end: usize){
+    if start >= end { return; }
+    let mut left = start + 1; 
+    let mut right = end - 1;
+    let pivot = list[start];
+    while left < right{
+        while left < end && list[left] < pivot  { left += 1; }
+        while right > start && list[right] > pivot { right -= 1; }
+        if left < right { swap(list, left, right); }
+    }
+    if right > start && list[right] < pivot {
+        swap(list, start, right);
+    }
+    quicksort_helper(list, start, right);
+    quicksort_helper(list, right + 1, end);
+}
+fn swap(list: &mut Vec<i32>, idx_a: usize, idx_b: usize){
+    if idx_a == idx_b { return; }
+    list[idx_a] = list[idx_a] ^ list[idx_b];
+    list[idx_b] = list[idx_b] ^ list[idx_a];
+    list[idx_a] = list[idx_a] ^ list[idx_b];
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use rand::prelude::*;
-use power_merge::{dividing_algorithms::bottom_up, merging_algorithms::quicksort_like::quicksort_like_merge};
+use power_merge::{dividing_algorithms::bottom_up, merging_algorithms::quicksort_like::quicksort_like_merge, quicksort};
 
 fn main() {
     let mut rng = rand::rng();
-    let mut vec: Vec<i32> = (0..2048).collect();
+    let mut vec: Vec<i32> = (0..16).collect();
     vec.shuffle(&mut rng);
     print!("before: [",);
     for num in vec.clone() {
         print!("{}, ", num.to_string());
     }
     println!("]");
-    bottom_up::merge_sort(&mut vec,quicksort_like_merge);
+    quicksort(&mut vec);
     print!("after: [",);
     for num in vec.clone() {
         print!("{}, ", num.to_string());
